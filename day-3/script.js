3c4daac63ab908d9513bb8867a7b266c0a2fe50c
abebaye <84613251+abebaye@users.noreply.github.com>
2021-11-11 14:24:02 -0800

    third commit

---
+++ day-3/script.js
@@ -0,0 +1,47 @@
+// home take challege
+
+
+// Ask for the user's first name
+// Ask for the user's last name
+// Log the user's first name to the console
+// Alert the user's last name
+
+// *BONUS*
+// Ask for the user's birthday
+// Ask the user to confirm their birthday input
+// Alert the user's birthday
+
+
+// const Fname = "abebaye"
+// const Lname = "aleme"
+// console.log(Fname)
+// // prompt(Fname)
+// alert(Lname)
+// const Bday = "25"
+// confirm(Bday)
+// alert(Bday)
+
+// var Fname = String(prompt("First Name"));
+// console.log(Fname);
+
+// const Fname = "First Name"
+// prompt(Fname)
+// console.log(Fname)
+
+document.getElementById("myButton").onclick = function () {
+    const FName = document.getElementById("myTextF").value;
+    const LName = document.getElementById("myTextL").value;
+    const BName = document.getElementById("myTextB").value;
+    alert(LName);
+    confirm(BName);
+    alert(BName);
+    console.log("Hello", FName);
+};
+
+
+
+
+
+
+
+
